62b103bb0a1eaf7b0799ef4e3db22a09337a930a
Itzmesuccess <164071129+Itzmesuccess@users.noreply.github.com>
2024-04-07 23:37:46 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+
+#include "CourseList.hpp"
+
+using namespace std;
+
+int main() 
+{
+    CourseList courseList;
+
+    int choice;
+
+    string courseCode;
+
+    int creditHours;
+
+    char grade;
+
+    do {
+        // Display menu
+
+        cout << "\nMenu:\n";
+
+        cout << "1. Enter course\n";
+
+        cout << "2. Delete course\n";
+
+        cout << "3. Delete all courses\n";
+
+        cout << "4. Display course list\n";
+
+        cout << "5. Exit\n";
+
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice) {
+
+        case 1:
+
+            cout << "Enter course code: ";
+
+            cin >> courseCode;
+
+            cout << "Enter credit hours: ";
+
+            cin >> creditHours;
+
+            cout << "Enter grade: ";
+
+            cin >> grade;
+
+            courseList.addCourse(courseCode, creditHours, grade);
+
+            cout << "Course added successfully.\n";
+
+            break;
+
+        case 2:
+
+            cout << "Which code do you want deleted: ";
+
+            cin >> courseCode;
+
+            courseList.deleteCourse(courseCode);
+
+            cout << "Course deleted successfully.\n";
+
+            break;
+
+        case 3:
+
+            courseList.deleteAllNodes();
+
+            cout << "All courses deleted successfully.\n";
+
+            break;
+
+        case 4:
+
+            cout << "Course List:\n";
+
+            courseList.displayList();
+
+            break;
+
+        case 5:
+
+            cout << "Exiting program.\n";
+
+            break;
+
+        default:
+
+            cout << "Wrong choice. Try again.\n";
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
