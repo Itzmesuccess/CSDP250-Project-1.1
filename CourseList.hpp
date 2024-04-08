8e1e18cd1d831cf4a98cd4a9335da823aca9c786
Itzmesuccess <164071129+Itzmesuccess@users.noreply.github.com>
2024-04-07 23:37:19 -0400

    Add files via upload

---
+++ CourseList.hpp
@@ -0,0 +1,41 @@
+#ifndef CourseList_hpp
+#define CourseList_hpp
+
+#include <string>
+
+using namespace std;
+
+class CourseList
+{
+private:
+
+    struct CourseNode
+    {
+        string courseCode;
+
+        int creditHours;
+
+        char grade;
+
+        CourseNode* next;
+    };
+
+    CourseNode* head;
+
+public:
+
+    CourseList();
+
+    ~CourseList();
+
+    void addCourse(const string& courseCode, int creditHours, char grade);
+
+    void deleteCourse(const string& courseCode);
+
+    void deleteAllNodes();
+
+    void displayList() const;
+
+};
+
+#endif 
